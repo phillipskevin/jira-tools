197f960a251cf47908935fe02006f19b40ccc301
Kevin Phillips <kevin@bitovi.com>
2020-03-13 20:51:07 -0500

    spacing

--- build.js
+++ build.js
@@ -4,32 +4,32 @@ const globalJS = require("steal-tools/lib/build/helpers/global").js;
 const baseNormalize = globalJS.normalize();
 
 const globalBuilds = stealTools.export({
-	steal: {
-		config: __dirname + "/package.json!npm",
-		main: "jira-tools/can",
-		map: {
-			"can-observable-array": "can-observable-array/dist/can-observable-array",
-			"can-observable-object": "can-observable-object/dist/can-observable-object",
-			"can-observable-mixin": "can-observable-mixin/dist/mixins",
-			"can-stache-element": "can-stache-element/dist/can-stache-element"
-		}
-	},
-	options: {
-		useNormalizedDependencies: false,
-		verbose: true
-	},
-	outputs: {
-		"+bundled-es core": {
-			addProcessShim: true,
-			dest: __dirname + "/extension/lib/can.js",
-			modules: ["jira-tools/can"],
-            minify: true
-		}
-	}
+  steal: {
+    config: __dirname + "/package.json!npm",
+    main: "jira-tools/can",
+    map: {
+      "can-observable-array": "can-observable-array/dist/can-observable-array",
+      "can-observable-object": "can-observable-object/dist/can-observable-object",
+      "can-observable-mixin": "can-observable-mixin/dist/mixins",
+      "can-stache-element": "can-stache-element/dist/can-stache-element"
+    }
+  },
+  options: {
+    useNormalizedDependencies: false,
+    verbose: true
+  },
+  outputs: {
+    "+bundled-es core": {
+      addProcessShim: true,
+      dest: __dirname + "/extension/lib/can.js",
+      modules: ["jira-tools/can"],
+      minify: true
+    }
+  }
 });
 
 globalBuilds.catch((e) => {
-	setTimeout(() => {
-		throw e;
-	}, 1);
+  setTimeout(() => {
+    throw e;
+  }, 1);
 });
